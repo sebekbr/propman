bc3e7c31902da130fe975d9cf1de6f127aafdf3a
SebekBr <sebastian.brodziak@gmail.com>
2021-10-08 00:09:10 +0200

    Users model mockup finished

--- PropMan/models.py
+++ PropMan/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-class Users(models.Model):
-    """A typical class defining a model, derived from the Model class."""
 
+class Users(models.Model):
     # Fields
+    id = models.AutoField(primary_key=True)
     login = models.CharField(max_length=45, help_text='User login')
     password = models.CharField(max_length=198, help_text='User password')
     name = models.CharField(max_length=45, help_text='User first name')
@@ -12,13 +12,13 @@ class Users(models.Model):
 
     # Metadata
     class Meta:
-        ordering = ['-my_field_name']
+        ordering = ['id']
 
     # Methods
-    def get_absolute_url(self):
-        """Returns the url to access a particular instance of MyModelName."""
-        return reverse('model-detail-view', args=[str(self.id)])
+    #def get_absolute_url(self):
+    #    """Returns the url to access a particular instance of MyModelName."""
+    #    return reverse('model-detail-view', args=[str(self.id)])
 
     def __str__(self):
         """String for representing the MyModelName object (in Admin site etc.)."""
-        return self.my_field_name
+        return self.id, self.login, self.password, self.name, self.surname, self.type
