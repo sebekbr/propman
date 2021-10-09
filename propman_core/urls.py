81d575e98ba8370d1cc034909d334813b6e6d3aa
SebekBr <sebastian.brodziak@gmail.com>
2021-10-09 21:33:50 +0200

    Started app in Django

--- propman_core/urls.py
+++ propman_core/urls.py
@@ -1,4 +1,4 @@
-"""PropMan URL Configuration
+"""propman_core URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
