50d8a7a70be08cc040cdaf78c95e38fb9067db74
SebekBr <sebastian.brodziak@gmail.com>
2021-10-09 22:25:01 +0200

    Restoring app to work

--- PropMan/wsgi.py
+++ PropMan/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for propman_core project.
+WSGI config for PropMan project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'propman_core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PropMan.settings')
 
 application = get_wsgi_application()
