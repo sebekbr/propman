a9eaef05e67b434b5298d9a8b1b2396a14194174
SebekBr <sebastian.brodziak@gmail.com>
2021-10-10 00:14:21 +0200

    Some changes

--- PropMan/urls.py
+++ PropMan/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView # new
 
 urlpatterns = [
     path('app/', include('app.urls')), # Podłączenie zewnętrznego widoku
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')), # Moduł autentykacji do logowania użytkownika
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
