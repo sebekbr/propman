ab96ba53fea45919b16f868ea280f43af68b51a5
SebekBr <sebastian.brodziak@gmail.com>
2021-10-13 00:05:46 +0200

    Home and base changes, including settings

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,6 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    #Add Django site authentication urls (for login, logout, password management)
-    path('accounts/', include('django.contrib.auth.urls')),
 ]
-
