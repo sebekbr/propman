1456ba71fa70ed09ea19988b788ac8bdef067a91
SebekBr <sebastian.brodziak@gmail.com>
2021-10-12 23:40:28 +0200

    Move base, home, login files to template dir, urls.py mod by adding auth method

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-
+    #Add Django site authentication urls (for login, logout, password management)
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
+
