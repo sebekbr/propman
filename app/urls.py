fb549f94aaaa80c75195d2493e194cd5535e2936
SebekBr <sebastian.brodziak@gmail.com>
2021-10-17 22:51:17 +0200

    Added form/list

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('form/list', views.index, name='form_list'),
     path('form/<int:pk>/', views.form_detail, name='form_detail'),
     path('forms/new', views.housingassociation_form_new, name='form_new'), # definicja adresu URL nowego wpisu
 ]
