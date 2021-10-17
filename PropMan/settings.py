367b48f013842f44706d64aa48fe486cd40c2e40
SebekBr <sebastian.brodziak@gmail.com>
2021-10-17 21:09:43 +0200

    changed INSTALLED_APPS entry

--- PropMan/settings.py
+++ PropMan/settings.py
@@ -32,13 +32,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'app.apps.PropmanConfig',
+    # 'app.apps.PropmanConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app'
 ]
 
 MIDDLEWARE = [
