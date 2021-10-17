0e839cac4de6488aab8590ccf537f4bdd8f7e96f
SebekBr <sebastian.brodziak@gmail.com>
2021-10-17 23:05:46 +0200

    Corrected url path form to forms

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('form/list', views.index, name='form_list'),
-    path('form/<int:pk>/', views.form_detail, name='form_detail'),
+    path('forms/list', views.form_list, name='ha_list'),
+    path('forms/<int:pk>/', views.form_detail, name='form_detail'),
     path('forms/new', views.housingassociation_form_new, name='form_new'), # definicja adresu URL nowego wpisu
 ]
