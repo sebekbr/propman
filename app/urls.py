55f4d1740c1fecd73a4e82150f9f9974d956fdb7
SebekBr <sebastian.brodziak@gmail.com>
2021-10-17 21:01:49 +0200

    changed name=form to form_new

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('post/<int:pk>/', views.form_detail, name='form_detail'),
-    path('forms/new', views.housingassociation_form_new, name='form'), # definicja adresu URL nowego wpisu
+    path('form/<int:pk>/', views.form_detail, name='form_detail'),
+    path('forms/new', views.housingassociation_form_new, name='form_new'), # definicja adresu URL nowego wpisu
 ]
