a1e8f20b2633dbb3cc784bf162f924c5b3b55c3d
SebekBr <sebastian.brodziak@gmail.com>
2021-10-17 18:48:58 +0200

    new path for form_detail added

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('post/<int:pk>/', views.form_detail, name='form_detail'),
     path('forms/new', views.housingassociation_form_new, name='form'), # definicja adresu URL nowego wpisu
 ]
