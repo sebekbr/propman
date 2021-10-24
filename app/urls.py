ace0b86310c1f9ae90d6383c28e9b25fe433693b
SebekBr <sebastian.brodziak@gmail.com>
2021-10-24 22:43:46 +0200

    some changes

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('forms/list', views.form_list, name='ha_list'),
     path('forms/<int:pk>/', views.form_detail, name='form_detail'),
     path('forms/new', views.housingassociation_form_new, name='form_new'), # definicja adresu URL nowego wpisu
+    # path('login/',auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    # path('join/', views.SignupView.as_view(), name='join'),
+    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
