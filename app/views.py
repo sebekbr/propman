b5a329721cfa8c65ea4f3e69869e8170765aa404
SebekBr <sebastian.brodziak@gmail.com>
2021-10-29 22:30:46 +0200

    Changed propman to registration in file path

--- app/views.py
+++ app/views.py
@@ -4,23 +4,23 @@ from .forms import *
 
 def index(request):
     # return HttpResponse("Hello, world. This is PropMan.")
-    return render(request, 'propman/index.html')
+    return render(request, 'registration/index.html')
 
 
 # Widok nowego wpisu
 # def housingassociation_form_new(request):
 #     form = HousingAssociationForm()
-#     return render(request, 'propman/form_edit.html', {'form': form})
+#     return render(request, 'registration/form_edit.html', {'form': form})
 
 
 def form_list(request):
     # form = get_object_or_404(HousingAssociationForm, pk=pk)
-    return render(request, 'propman/ha_list.html')
+    return render(request, 'registration/ha_list.html')
 
 
 def form_detail(request, pk):
     form = get_object_or_404(HousingAssociationForm, pk=pk)
-    return render(request, 'propman/form_detail.html', {'form': form})
+    return render(request, 'registration/form_detail.html', {'form': form})
 
 
 # Zapisywanie formularza
@@ -32,5 +32,5 @@ def housingassociation_form_new(request):
             return redirect('form_detail', pk=HousingAssociation.id)
         else:
             form = HousingAssociationForm()
-        return render(request, 'propman/form_edit.html', {'form': form})
+        return render(request, 'registration/form_new.html', {'form': form})
 
