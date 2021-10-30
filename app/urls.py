1f3fcd0ea113004e352302cc7c43403d52258a1a
SebekBr <sebastian.brodziak@gmail.com>
2021-10-30 07:37:40 +0200

    Changed form alias to housingassociation in urls and views

--- app/urls.py
+++ app/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('forms/list', views.form_list, name='ha_list'),
-    path('forms/<int:pk>/', views.form_detail, name='form_detail'),
-    path('forms/new', views.housingassociation_form_new, name='form_new'), # definicja adresu URL nowego wpisu
+    path('forms/<int:pk>', views.form_detail, name='form_detail'),
+    path('forms/housingassociation_new', views.housingassociation_form_new, name='housingassociation_new'), # definicja adresu URL nowego wpisu
     # path('login/',auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     # path('join/', views.SignupView.as_view(), name='join'),
     # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
--- app/views.py
+++ app/views.py
@@ -29,8 +29,8 @@ def housingassociation_form_new(request):
         form = HousingAssociationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('form_detail', pk=HousingAssociation.id)
+            return redirect('housingassociation_detail', pk=HousingAssociation.id)
     else:
         form = HousingAssociationForm()
-        return render(request, 'registration/form_new.html', {'form': form})
+        return render(request, 'registration/ha_new.html', {'housingassociation': form})
 
