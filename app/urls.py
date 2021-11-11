0c77f369608b611fce773b62174f5feb702371a8
SebekBr <sebastian.brodziak@gmail.com>
2021-11-11 23:08:58 +0100

    Working detail, fix EDIT needed

--- app/urls.py
+++ app/urls.py
@@ -5,10 +5,12 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('forms/housingassociation/list', views.form_list, name='ha_list'),
     path('forms/housingassociation/list/', views.form_list, name='ha_list'),
-    path('forms/housingassociation/item/<int:pk>', views.housingassociation_detail, name='ha_detail'),
-    path('forms/housingassociation/item/<int:pk>/', views.housingassociation_detail, name='ha_detail'),
     path('forms/housingassociation/new', views.housingassociation_form_new, name='housingassociation_new'), # definicja adresu URL nowego wpisu
     path('forms/housingassociation/new/', views.housingassociation_form_new, name='housingassociation_new'),
+    path('forms/housingassociation/item/<int:pk>', views.housingassociation_detail, name='ha_detail'),
+    path('forms/housingassociation/item/<int:pk>/', views.housingassociation_detail, name='ha_detail'),
+    path('forms/housingassociation/item/<int:pk>/edit', views.housingassociation_form_edit, name='ha_edit'),
+    path('forms/housingassociation/item/<int:pk>/edit/', views.housingassociation_form_edit, name='ha_edit'),
     path('forms/housingassociation/success', views.housingassociation_success, name='ha_success')
     # path('login/',auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     # path('join/', views.SignupView.as_view(), name='join'),
--- app/views.py
+++ app/views.py
@@ -23,27 +23,24 @@ def housingassociation_form_new(request):
         form = HousingAssociationForm(request.POST)
         if form.is_valid():
             form.save()
-            # messages.success(request, 'Pomyślnie utworzono!')
             return render(request, 'registration/success.html')
     else:
         form = HousingAssociationForm()
         return render(request, 'registration/ha_new.html', {'housingassociation_new': form})
 
 
+# Edycja wpisu
+def housingassociation_form_edit(request, pk):
+    detail = get_object_or_404(HousingAssociation, pk=pk)
+    if request.method == "POST":
+        form = HousingAssociationForm(request.POST, instance=detail)
+        if form.is_valid():
+            form.save()
+            return redirect('ha_edit')
+    return render(request, 'registration/ha_edit.html', {'ha_edit': form})
+
+
 def housingassociation_success(request):
     # ha_detail = get_object_or_404(HousingAssociation, pk=pk)
     return render(request, 'registration/ha_success.html', {'ha_success': housingassociation_success})
 
-
-# Edycja wpisu
-# def housingassociation_form_edit(request):
-#     ha_detail = get_object_or_404(HousingAssociation, pk=pk)
-#     if request.method == "POST":
-#         form = HousingAssociationForm(request.POST, instance=ha_detail)
-#         if form.is_valid():
-#             ha_detail = form.save(commit=False)
-#             ha_detail.name =
-#             return redirect('housingassociation_detail', pk=HousingAssociation.pk)
-#     else:
-#         form = HousingAssociationForm()
-#         return render(request, 'registration/ha_new.html', {'housingassociation_new': form})
