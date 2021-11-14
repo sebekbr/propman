c394ac1ce114c08de618b4f60dc1389b0e660a00
SebekBr <sebastian.brodziak@gmail.com>
2021-11-14 17:22:28 +0100

    Tenant methods added

--- app/views.py
+++ app/views.py
@@ -7,14 +7,15 @@ def index(request):
     return render(request, 'registration/index.html')
 
 
-def form_list(request):
-    ha_all = HousingAssociation.objects.all()
-    return render(request, 'registration/ha_list.html', {'ha_all': ha_all})
+# Housing Association methods
+def ha_list(request):
+    form = HousingAssociation.objects.all()
+    return render(request, 'registration/ha_list.html', {'ha_all': form})
 
 
 def housingassociation_detail(request, pk):
-    ha_detail = get_object_or_404(HousingAssociation, pk=pk)
-    return render(request, 'registration/ha_detail.html', {'ha_detail': ha_detail})
+    form = get_object_or_404(HousingAssociation, pk=pk)
+    return render(request, 'registration/ha_detail.html', {'ha_detail': form})
 
 
 # Zapisywanie formularza
@@ -43,6 +44,44 @@ def housingassociation_form_edit(request, pk):
 
 
 def housingassociation_success(request):
-    # ha_detail = get_object_or_404(HousingAssociation, pk=pk)
     return render(request, 'registration/ha_success.html', {'ha_success': housingassociation_success})
 
+
+# Tenants methods
+def tenant_list(request):
+    form = Tenants.objects.all()
+    return render(request, 'registration/tenant_list.html', {'tenant_all': form})
+
+
+def tenant_detail(request, pk):
+    form = get_object_or_404(Tenants, pk=pk)
+    return render(request, 'registration/tenant_detail.html', {'tenant_detail': form})
+
+
+# Zapisywanie formularza
+def tenant_form_new(request):
+    if request.method == "POST":
+        form = TenantsForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'registration/tenant_new_success.html') # Render strony po pomyślnym utworzeniu
+    else:
+        form = HousingAssociationForm()
+        return render(request, 'registration/tenant_new.html', {'tenant_new': form})
+
+
+# Edycja wpisu
+def tenant_form_edit(request, pk):
+    detail = get_object_or_404(Tenants, pk=pk)
+    if request.method == "POST":
+        form = TenantsForm(request.POST, instance=detail)
+        if form.is_valid():
+            form.save()
+            return render(request, 'registration/tenant_edit_success.html') # Render strony po pomyślnym zapisaniu
+    else:
+        form = TenantsForm(instance=detail)
+    return render(request, 'registration/ha_edit.html', {'ha_edit': form})
+
+
+def tenant_success(request):
+    return render(request, 'registration/tenant_success.html', {'tenant_success': tenant_success})
