0032bde09bdccdbbd029a7d5eca905cbc206b2a6
SebekBr <sebastian.brodziak@gmail.com>
2021-11-16 00:53:07 +0100

    URLs added

--- app/urls.py
+++ app/urls.py
@@ -24,17 +24,62 @@ urlpatterns = [
     path('forms/tenant/item/<int:pk>/', views.tenant_detail, name='tenant_detail'),
     path('forms/tenant/item/<int:pk>/edit', views.tenant_form_edit, name='tenant_edit'),
     path('forms/tenant/item/<int:pk>/edit/', views.tenant_form_edit, name='tenant_edit'),
-    path('forms/tenant/success', views.tenant_success, name='tenant_success')
+    path('forms/tenant/success', views.tenant_success, name='tenant_success'),
 
     # Landlords
+    path('forms/landlord/list', views.landlord_list, name='landlord_list'),
+    path('forms/landlord/list/', views.landlord_list, name='landlord_list'),
+    path('forms/landlord/new', views.landlord_form_new, name='landlord_new'), # definicja adresu URL nowego wpisu
+    path('forms/landlord/new/', views.landlord_form_new, name='landlord_new'),
+    path('forms/landlord/item/<int:pk>', views.landlord_detail, name='landlord_detail'),
+    path('forms/landlord/item/<int:pk>/', views.landlord_detail, name='landlord_detail'),
+    path('forms/landlord/item/<int:pk>/edit', views.landlord_form_edit, name='landlord_edit'),
+    path('forms/landlord/item/<int:pk>/edit/', views.landlord_form_edit, name='landlord_edit'),
+    path('forms/landlord/success', views.landlord_success, name='landlord_success'),
 
     # Property
+    path('forms/property/list', views.property_list, name='property_list'),
+    path('forms/property/list/', views.property_list, name='property_list'),
+    path('forms/property/new', views.property_form_new, name='property_new'), # definicja adresu URL nowego wpisu
+    path('forms/property/new/', views.property_form_new, name='property_new'),
+    path('forms/property/item/<int:pk>', views.property_detail, name='property_detail'),
+    path('forms/property/item/<int:pk>/', views.property_detail, name='property_detail'),
+    path('forms/property/item/<int:pk>/edit', views.property_form_edit, name='property_edit'),
+    path('forms/property/item/<int:pk>/edit/', views.property_form_edit, name='property_edit'),
+    path('forms/property/success', views.property_success, name='property_success'),
 
     # Lease Agreements
+    path('forms/leaseagreement/list', views.leaseagreement_list, name='leaseagreement_list'),
+    path('forms/leaseagreement/list/', views.leaseagreement_list, name='leaseagreement_list'),
+    path('forms/leaseagreement/new', views.leaseagreement_form_new(), name='leaseagreement_new'), # definicja adresu URL nowego wpisu
+    path('forms/leaseagreement/new/', views.leaseagreement_form_new(), name='leaseagreement_new'),
+    path('forms/leaseagreement/item/<int:pk>', views.leaseagreement_detail, name='leaseagreement_detail'),
+    path('forms/leaseagreement/item/<int:pk>/', views.leaseagreement_detail, name='leaseagreement_detail'),
+    path('forms/leaseagreement/item/<int:pk>/edit', views.leaseagreement_form_edit, name='leaseagreement_edit'),
+    path('forms/leaseagreement/item/<int:pk>/edit/', views.leaseagreement_form_edit, name='leaseagreement_edit'),
+    path('forms/leaseagreement/success', views.leaseagreement_success, name='leaseagreement_success'),
 
     # Bill Vendors
+    path('forms/billven/list', views.billvendor_list, name='billvendor_list'),
+    path('forms/billven/list/', views.billvendor_list, name='billvendor_list'),
+    path('forms/billven/new', views.billvendor_form_new, name='billvendor_new'), # definicja adresu URL nowego wpisu
+    path('forms/billven/new/', views.billvendor_form_new, name='billvendor_new'),
+    path('forms/billven/item/<int:pk>', views.billvendor_detail, name='billvendor_detail'),
+    path('forms/billven/item/<int:pk>/', views.billvendor_detail, name='billvendor_detail'),
+    path('forms/billven/item/<int:pk>/edit', views.billvendor_form_edit, name='billvendor_edit'),
+    path('forms/billven/item/<int:pk>/edit/', views.billvendor_form_edit, name='billvendor_edit'),
+    path('forms/billven/success', views.billvendor_success, name='billvendor_success'),
 
     # Bills
+    path('forms/bill/list', views.bill_list, name='bill_list'),
+    path('forms/bill/list/', views.bill_list, name='bill_list'),
+    path('forms/bill/new', views.bill_form_new, name='bill_new'), # definicja adresu URL nowego wpisu
+    path('forms/bill/new/', views.bill_form_new, name='bill_new'),
+    path('forms/bill/item/<int:pk>', views.bill_detail, name='bill_detail'),
+    path('forms/bill/item/<int:pk>/', views.bill_detail, name='bill_detail'),
+    path('forms/bill/item/<int:pk>/edit', views.bill_form_edit, name='bill_edit'),
+    path('forms/bill/item/<int:pk>/edit/', views.bill_form_edit, name='bill_edit'),
+    path('forms/billtenant/success', views.bill_success, name='bill_success')
 
     #
     # path('login/',auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
