45cd65bd6cf75158d6bfd5ada4ea1be6a44ca0c9
SebekBr <sebastian.brodziak@gmail.com>
2021-12-11 23:48:21 +0100

    added context processors

---
+++ app/context_processors.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from app.models import *
+
+
+def counter(request):
+    prop_count = Property.objects.count()
+    la_count = LeaseAgreement.objects.count()
+    ha_count = HousingAssociation.objects.count()
+    return render(request, 'registration/ha/ha_list.html', {'property_count': prop_count,
+                                                            'agreement_count': la_count,
+                                                            'ha_count': ha_count})
+
