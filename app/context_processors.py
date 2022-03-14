d432a82f54f91624bcda7f0c4ed2d5d8eae500b3
SebekBr <sebastian.brodziak@gmail.com>
2022-03-14 05:14:23 +0100

    own context_processors delete

--- app/context_processors.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from app.models import *
-
-
-def counter(request):
-    prop_count = Property.objects.count()  # Zliczanie ilości obiektów w tablicy
-    return render(request, 'registration/prop/prop_list.html', {'property_count': prop_count})
-
